6cfe1d5b40339a4981165583497a47acf371ebdd
BrandonJafeth <152206866+BrandonJafeth@users.noreply.github.com>
2024-05-16 00:44:42 -0600

    Add files via upload

---
+++ Entities/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        // Relación
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+}
