3262c620ff594ebd260a60b7f72c1b5c388f4a56
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-05-19 15:16:47 -0600

    Cambios minimos

--- Entities/Patient.cs
+++ Entities/Patient.cs
@@ -4,10 +4,12 @@
     {
         public int Id_Patient { get; set; }
         public string Pat_Name { get; set; }
-        public string Email { get; set; }
+        
         public string Phone { get; set; }
 
         // Relations
+        public User User { get; set; }
+        public string Email => User?.Email;
         public ICollection<Appointment> Appointments { get; set; }
     }
 }
