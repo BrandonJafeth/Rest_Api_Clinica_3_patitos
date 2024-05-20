39e938effb9a288c535171434834506ad6ec2be6
Dian <dian7875.a@gmail.com>
2024-05-20 15:40:18 -0600

    Test2

--- Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7925c433a46728944d8603eb580c590e26d280")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca59306532f8b1778d76128db06fef83d728cc17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
