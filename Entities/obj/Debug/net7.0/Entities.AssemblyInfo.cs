ed1c78547b79fb2f7fe2d6d6babf35cc09047ffa
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-05-20 01:29:14 -0600

    Eliminación de el controlador del cilma y publicación del backup

--- Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307866cfb605dff2dbac55a66fe7eb596b75b78b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c1adab1a44eb09ad17832988bd4cd373783e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
