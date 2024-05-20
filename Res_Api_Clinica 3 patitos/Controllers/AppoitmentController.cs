eb6a8d81591dc933c51de0bd186b46a5e1a02e73
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-05-20 01:42:58 -0600

    Eliminar y agregar controlador de base nada mas aun no tiene logica

---
+++ Res_Api_Clinica 3 patitos/Controllers/AppoitmentController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Res_Api_Clinica_3_patitos.Controllers
+{
+    public class AppoitmentController : Controller
+    {
+        // GET: AppoitmentController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AppoitmentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AppoitmentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppoitmentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AppoitmentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AppoitmentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AppoitmentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AppoitmentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
