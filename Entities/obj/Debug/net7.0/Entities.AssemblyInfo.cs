8053d24d0ece5a7383dd3de126a730c1c42185b1
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-05-21 14:21:16 -0600

    Unos cambios en el program

--- Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173273caace3145757c4401dc5e6a571f198363e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac8908051a898a0ba4a832eebb67953e6bce4e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
